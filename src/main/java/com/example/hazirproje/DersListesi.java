eefc5e784d18f97376d7ab43e558c1ea4d2db319
KKayaSafa <safakilickaya34@gmail.com>
2024-01-02 20:43:22 +0300

    ders listesine örnek ders eklendi

--- src/main/java/com/example/hazirproje/DersListesi.java
+++ src/main/java/com/example/hazirproje/DersListesi.java
@@ -13,11 +13,27 @@ public class DersListesi {
         mevcutDersler = new ArrayList<>();
         gecilmisDersler = new ArrayList<>();
 
-        GecilmisDers g = new GecilmisDers("Kimya","Temel Üniversite Kimyası","KIM2002",6,3,40,60);
-        gecilmisDersler.add(g);
+        // Arayüzde başlangıçta görüntülenecek örnek dersler
 
-        MevcutDers m = new MevcutDers("Bilgisayar Programlama 2","Python Programlama Dili","BLM2002",6,3,"Doç.Dr. Ayşe Berna Altınel Girgin");
-        mevcutDersler.add(m);
+        GecilmisDers g1 = new GecilmisDers("Kimya","Temel Üniversite Kimyası","KIM2002",6,3,40,60);
+        gecilmisDersler.add(g1);
+
+        GecilmisDers g2 = new GecilmisDers("Matematik 1","Calculus 1","MAT101",5,1,70,90);
+        gecilmisDersler.add(g2);
+
+        GecilmisDers g3 = new GecilmisDers("Bilgisayar Donanımı","Bilgisayarın fiziksel bileşenlerinin çalışma prensibi","BLM1004",5,2,90,64);
+        gecilmisDersler.add(g3);
+
+
+
+        MevcutDers m1 = new MevcutDers("Bilgisayar Programlama 2","Python Programlama Dili","BLM2002",6,3,"Doç.Dr. Ayşe Berna Altınel Girgin");
+        mevcutDersler.add(m1);
+
+        MevcutDers m2 = new MevcutDers("Diferansiyel Denklemler","Diferansiyel denklemlerin çözümleri","MAT201",4,3,"Doç.Dr. Burhan Tiryakioğlu");
+        mevcutDersler.add(m2);
+
+        MevcutDers m3 = new MevcutDers("Nesne Yönelimli Programlama","Java programlama dili","BLM2005",6,3,"Dr. Eyüp Emre Ülkü");
+        mevcutDersler.add(m3);
 
 
 
